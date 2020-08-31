9a4edeae78647d8eb165d2d6027c7249e340c859
siriusmwx <49541321+siriusmwx@users.noreply.github.com>
2020-08-31 21:08:58 +0800

    Add files via upload

--- twitter-video-downloader/twitter-dl.py
+++ twitter-video-downloader/twitter-dl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import re
 import json
 import m3u8
@@ -7,7 +7,7 @@ import argparse
 import requests
 import urllib.parse
 from pathlib import Path
-from subprocess import run
+from subprocess import run, DEVNULL
 
 
 class TwitterDownloader:
@@ -18,7 +18,7 @@ class TwitterDownloader:
     video_player_prefix = 'https://twitter.com/i/videos/tweet/'
     video_api = 'https://api.twitter.com/1.1/videos/tweet/config/'
 
-    def __init__(self, tweet_url, output_dir='output', debug=0):
+    def __init__(self, tweet_url, output_dir='output', debug=1, proxy=False):
         """
         We split on ? to clean up the URL. Sharing tweets, for example,
         will add ? with data about which device shared it.
@@ -29,13 +29,22 @@ class TwitterDownloader:
         self.tweet_data['user'] = self.tweet_data['tweet_url'].split('/')[3]
         self.tweet_data['id'] = self.tweet_data['tweet_url'].split('/')[5]
 
-        storage_dir = Path(output_dir) / self.tweet_data['user']
-        Path.mkdir(storage_dir, parents=True, exist_ok=True)
-        self.storage_dir = str(storage_dir)
+        storage_dir = Path(output_dir, self.tweet_data['user'])
+        storage_dir.mkdir(parents=True, exist_ok=True)
+        self.storage_dir = str(storage_dir.absolute())
 
         self.request = requests.Session()
-        # self.request.proxies = {'http': 'socks5://127.0.0.1:1080',
-        #                         'https': 'socks5://127.0.0.1:1080'}
+        if proxy:
+            # - SOCKS4A (``proxy_url='socks4a://...``)
+            # - SOCKS4 (``proxy_url='socks4://...``)
+            # - SOCKS5 with remote DNS (``proxy_url='socks5h://...``)
+            # - SOCKS5 with local DNS (``proxy_url='socks5://...``)
+            self.request.proxies = {'http': 'socks5h://127.0.0.1:1080',
+                                    'https': 'socks5h://127.0.0.1:1080'}
+            # import socks
+            # import socket
+            # socks.set_default_proxy(socks.SOCKS5, '127.0.0.1', 1080)
+            # socket.socket = socks.socksocket # does not works
 
     def download(self):
         self.__debug('Tweet URL', self.tweet_data['tweet_url'])
@@ -62,10 +71,8 @@ class TwitterDownloader:
         match = re.search(
             '/([^/]+)/([^/]+).m3u8', playlist_url)
         video_name = match.group(2) + '_' + match.group(1)
-        video_file = str(Path(self.storage_dir) /
-                         Path(video_name + '.mp4'))
-        ts_full_file = str(Path(self.storage_dir) /
-                           Path(video_name + '.ts'))
+        video_file = Path(self.storage_dir, video_name + '.mp4')
+        ts_full_file = Path(self.storage_dir, video_name + '.ts')
 
         print('[+] Downloading ' + video_name + '.mp4')
         resp = self.request.get(
@@ -75,25 +82,26 @@ class TwitterDownloader:
         for ts_uri in ts_m3u8_parse.segments.uri:
             ts_file = self.request.get(video_host + ts_uri)
             fname = ts_uri.split('/')[-1]
-            ts_path = Path(self.storage_dir) / Path(fname)
+            ts_path = Path(self.storage_dir, fname)
             ts_list.append(ts_path)
             ts_path.write_bytes(ts_file.content)
 
-        with open(ts_full_file, 'wb') as wfd:
-            for f in ts_list:
-                with open(f, 'rb') as fd:
+        with open(str(ts_full_file), 'wb') as wfd:
+            for ts in ts_list:
+                with open(str(ts), 'rb') as fd:
                     shutil.copyfileobj(fd, wfd, 1024 * 1024 * 10)
 
         print('\t[*] Doing the magic ...')
-        # cmd = 'ffmpeg -y -i %s -c:v libx264 -c:a copy -bsf:a aac_adtstoasc %s' % (
-        #     ts_full_file, video_file)
-        cmd = 'ffmpeg -y -i %s -acodec copy -vcodec copy -f mp4 %s' % (
-            ts_full_file, video_file)
-        proc = run(cmd, capture_output=True, shell=True)
+        ffmpeg = Path('ffmpeg').absolute()
+        # cmd = '%s -y -i %s -c:v libx264 -c:a copy -bsf:a aac_adtstoasc %s' % (
+        #     ffmpeg, ts_full_file, video_file)
+        cmd = '%s -y -i %s -acodec copy -vcodec copy -f mp4 %s' % (
+            ffmpeg, ts_full_file, video_file)
+        proc = run(cmd, stdout=DEVNULL, stderr=DEVNULL, shell=True)
 
         print('\t[+] Doing cleanup...')
-        [Path(ts).unlink() for ts in ts_list]
-        Path(ts_full_file).unlink()
+        [ts.unlink() for ts in ts_list]
+        ts_full_file.unlink()
 
     def __get_bearer_token(self):
         video_player_url = self.video_player_prefix + self.tweet_data['id']
@@ -137,10 +145,6 @@ class TwitterDownloader:
         return video_host, m3u8_parse
 
     def __get_guest_token(self):
-        """
-        Thanks to @devkarim for this fix: 
-        https://github.com/h4ckninja/twitter-video-downloader/issues/2#issuecomment-538773026
-        """
         res = self.request.post(
             "https://api.twitter.com/1.1/guest/activate.json")
         res_json = json.loads(res.text)
@@ -168,8 +172,11 @@ def main():
                         help='The directory to download videos.(<output>/<user>/)')
     parser.add_argument('-d', '--debug', choices=[0, 1, 2], default=1, type=int,
                         help='Debug level. Show more information of response bodies.')
+    parser.add_argument('-p', '--proxy', action='store_true',
+                        help='Download video with socks5 proxy,default=127.0.0.1:1080.')
     args = parser.parse_args()
-    twitter_dl = TwitterDownloader(args.tweet_url, args.output, args.debug)
+    twitter_dl = TwitterDownloader(
+        args.tweet_url, args.output, args.debug, args.proxy)
     twitter_dl.download()
 
 if __name__ == '__main__':
