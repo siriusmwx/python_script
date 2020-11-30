28762fd413b90447696b13c221ecf775e1120fd0
siriusmwx <49541321+siriusmwx@users.noreply.github.com>
2020-11-30 21:34:59 +0800

    Add files via upload

--- remote_hosts.py
+++ remote_hosts.py
@@ -1,17 +1,50 @@
 import os
 import sys
+import re
 import csv
 import time
 import socket
 import threading
+import argparse
 from subprocess import run
+
+# Console colors
+W = '\033[0m'  # white (normal)
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+O = '\033[33m'  # orange
+B = '\033[34m'  # blue
+P = '\033[35m'  # purple
+C = '\033[36m'  # cyan
+GR = '\033[37m'  # gray
 
 
 def get_host_ip():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.connect(('172.24.255.212', 80))
         ip = sock.getsockname()[0]
+    print(f'Your host IP is {O}{ip}{W}')
     return ip
+
+
+def host_status(host_ip):
+    proc = run(['ping', host_ip, '-n', '2'],
+               capture_output=True, universal_newlines=True)
+    if 'TTL=' in proc.stdout:
+        active_ips.add(host_ip)
+        print(f"{G}{host_ip}{W} is {O}online{W}")
+
+
+def ping_ips():
+    start_time = time.time()
+    for ip in ip_list:
+        t = threading.Thread(target=host_status, args=(ip,), daemon=True)
+        t.start()
+        # while True:
+        #     if len(threading.enumerate()) < 50:
+        #         break
+    while len(threading.enumerate()) > 1 and time.time() - start_time <= 60:
+        pass
 
 
 def create_magic_packet(macaddress):
@@ -74,70 +107,65 @@ class Host():
     def __init__(self, host_mac, host_ip):
         self.mac = host_mac
         self.ip = host_ip
-        # self.name = host_name
         self.user = ""
         self.passwd = ""
         self.status = ""
+        self.model = ""
 
-
-def host_status(host_ip):
-    proc = run(['ping', host_ip, '-n', '2'],
-               capture_output=True, universal_newlines=True)
-    if 'TTL=' in proc.stdout:
-        active_ips.add(host_ip)
-        print("%s is online" % host_ip)
-
-
-def ping_ips():
-    start_time = time.time()
-    for ip in ip_list:
-        t = threading.Thread(target=host_status, args=(ip,), daemon=True)
-        t.start()
-        # while True:
-        #     if len(threading.enumerate()) < 50:
-        #         break
-    while len(threading.enumerate()) > 1 and time.time() - start_time <= 60:
-        pass
-
-
-def magic_wakeon(host):
-    if not host.status:
-        # proc = run('mc-wol %s' % host.mac, capture_output=True)
-        send_magic_packet(host.mac, port=8384)
-        print('Send magic packet to %s<==>%s' % (host.ip, host.mac))
-        return True
-    print("The host %s<==>%s is already power on" % (host.ip, host.mac))
-
-
-def remote_shutdown(host):
-    if host.status:
-        if host.user and host.passwd:
-            proc = run('net use \\\\%s %s /user:%s' % (
-                host.ip, host.passwd, host.user), capture_output=True)
-            if proc.returncode != 0:
-                print('Fail to power off %s<==>%s' % (host.ip, host.mac))
+    def remote_host(self, action=None, model=None):
+        if model:
+            if not model.search(self.model):
                 return
-            proc = run('shutdown /m \\\\%s /s /f /t 0' %
-                       host.ip, capture_output=True)
-            if proc.returncode != 0:
-                print('Fail to power off %s<==>%s' % (host.ip, host.mac))
-                return
-            print('Success to power off %s<==>%s' % (host.ip, host.mac))
+        if action:
+            # print(f'Starting remote hosts to {O}power on{W}')
+            self.magic_wakeon()
+        else:
+            # print(f'Starting remote hosts to {O}power off{W}')
+            self.remote_shutdown()
+
+    def magic_wakeon(self):
+        if self.ip not in active_ips:
+            self.status = 'OFF'
+            # proc = run('mc-wol %s' % self.mac, capture_output=True)
+            send_magic_packet(self.mac, port=8384)
+            print(f'Send magic packet to {G}{self.ip}{W}<==>{O}{self.mac}{W}')
             return True
-    else:
-        print("The host %s<==>%s is already power off" % (host.ip, host.mac))
+        self.status = 'ON'
+        print(f"The host {G}{self.ip}{W}<==>{O}{self.mac}{W} is already {C}power on{W}")
+
+    def remote_shutdown(self):
+        if self.ip in active_ips:
+            self.status = 'ON'
+            if self.user and self.passwd:
+                proc = run('net use \\\\%s %s /user:%s' % (
+                    self.ip, self.passwd, self.user), capture_output=True)
+                if proc.returncode != 0:
+                    print(f'{R}Fail{W} to remote host {G}{self.ip}{W}<==>{O}{self.mac}{W}')
+                    return
+                proc = run('shutdown /m \\\\%s /s /f /t 0' %
+                           self.ip, capture_output=True)
+                if proc.returncode != 0:
+                    print(f'{R}Fail{W} to power off {G}{self.ip}{W}<==>{O}{self.mac}{W}')
+                    return
+                print(f'{G}Success{W} to power off {G}{self.ip}{W}<==>{O}{self.mac}{W}')
+                self.status = 'OFF'
+                return True
+        else:
+            self.status = 'OFF'
+            print(f"The host {G}{self.ip}{W}<==>{O}{self.mac}{W} is already {C}power off{W}")
 
 
 def update_hosts():
-    print('Starting update hosts from csv file')
     hosts = {}
-    if os.path.exists('hosts.csv'):
-        with open('hosts.csv') as f:
+    if os.path.exists(csv_file):
+        print(f'Starting update hosts from {O}{csv_file}{W} file')
+        with open(csv_file) as f:
             csv_reader = csv.DictReader(f)
             for host in csv_reader:
                 hosts[host['MAC']] = Host(host['MAC'], host['IP'])
                 hosts[host['MAC']].user = host['USER']
                 hosts[host['MAC']].passwd = host['PASSWORD']
+                hosts[host['MAC']].model = host['MODEL']
 
     proc = run(['arp', '-a'], capture_output=True, universal_newlines=True)
     for result in proc.stdout.split('\n'):
@@ -150,28 +178,16 @@ def update_hosts():
             else:
                 hosts[host_mac].mac = host_mac
                 hosts[host_mac].ip = host_ip
-            hosts[host_mac].status = "ON"
     host_list = list(hosts.values())
     host_list.sort(key=lambda x: int(x.ip.split('.')[-1]))
     return host_list
 
 
-def remote_hosts(hosts, action=None):
-    if action == 'on':
-        print('Starting remote hosts to power on')
-        for host in hosts:
-            magic_wakeon(host)
-    else:
-        print('Starting remote hosts to power off')
-        for host in hosts:
-            remote_shutdown(host)
-
-
 def update_csv(host_list):
-    print('Starting update hosts to csv file')
-    with open('hosts.csv', 'w', newline='') as f:
+    print(f'Starting update hosts to {O}{csv_file}{W} file')
+    with open(csv_file, 'w', newline='') as f:
         csv_writer = csv.DictWriter(
-            f, fieldnames=['IP', 'MAC', 'USER', 'PASSWORD', 'STATUS'])
+            f, fieldnames=['IP', 'MAC', 'USER', 'PASSWORD', 'STATUS', 'MODEL'])
         csv_writer.writeheader()
         for host in host_list:
             csv_writer.writerow({
@@ -179,22 +195,75 @@ def update_csv(host_list):
                 'IP': host.ip,
                 'USER': host.user,
                 'PASSWORD': host.passwd,
-                'STATUS': host.status
+                'STATUS': host.status,
+                'MODEL': host.model
             })
 
 
+def arg_parser():
+    parser = argparse.ArgumentParser(
+        description='a simple script use to remote hosts to power on or off')
+    group1 = parser.add_mutually_exclusive_group()
+    group1.add_argument('-on', '--on', action='store_true',
+                        help='使用此选项用于远程启动计算机')
+    group1.add_argument('-off', '--off', action='store_true',
+                        help='使用此选项用于远程关闭计算机')
+    parser.add_argument('--model', action='store', nargs='+',
+                        help='指定需要执行远程启动或关闭的机种，可为多个机种')
+    parser.add_argument(
+        "--mac",
+        metavar="MAC",
+        nargs="+",
+        help="The mac addresses or of the computers you are trying to wake."
+    )
+    parser.add_argument(
+        "-i",
+        metavar="ip",
+        default="255.255.255.255",
+        help="The ip address of the host to send the magic packet to "
+        "(default 255.255.255.255)."
+    )
+    parser.add_argument(
+        "-p",
+        metavar="port",
+        type=int,
+        default=9,
+        help="The port of the host to send the magic packet to "
+        "(default 9)."
+    )
+    return parser
+
+
 if __name__ == '__main__':
+    parser = arg_parser()
+    args = parser.parse_args()
+    os.system('cls')
     active_ips = set()
     ip_header = ('.').join(get_host_ip().split('.')[0:3])
+    csv_file = ip_header.replace('.', '_') + '.csv'
     ip_list = ['%s.%s' % (ip_header, x) for x in range(1, 255)]
     try:
         ping_ips()
         hosts = update_hosts()
+        model = ""
+        if args.on:
+            if args.model:
+                print(f"The select model is {O}{' '.join(args.model)}{W}")
+                model = re.compile('|'.join(args.model), re.I)
+            print(f'Starting remote hosts to {O}power on{W}')
+            for host in hosts:
+                host.remote_host(action=True, model=model)
+        elif args.off:
+            if args.model:
+                print(f"The select model is {O}{' '.join(args.model)}{W}")
+                model = re.compile('|'.join(args.model), re.I)
+            print(f'Starting remote hosts to {O}power off{W}')
+            for host in hosts:
+                host.remote_host(model=model)
+        elif args.mac:
+            send_magic_packet(
+                *args.mac, ip_address=args.i, port=args.p)
         update_csv(hosts)
-        if len(sys.argv) == 2:
-            remote_hosts(hosts, action=sys.argv[1])
-        else:
-            sys.exit("args must be 'on' or 'off'")
     except KeyboardInterrupt:
         sys.exit()
     except Exception as e:
