70cc520572ea5bae8fd06e201212846ceec7db97
siriusmwx <siriusmwx@live.com>
2023-04-22 20:17:47 +0800

    2023年 04月 22日 星期六 20:17:47 CST

--- twitter-video-downloader/gmap_downloader.py
+++ twitter-video-downloader/gmap_downloader.py
@@ -1,11 +1,14 @@
-import math
 import os
+import sys
+import time
+import math
 import requests
 import cv2
 import numpy as np
 
 currend_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(currend_dir)
+map_dir = os.path.join(currend_dir, "map")
 
 
 def download(x, y, z, path):
@@ -15,17 +18,24 @@ def download(x, y, z, path):
     }
     url = f"https://mt.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
     path = os.path.join(path, f"{z}/{x}")
+    filepath = os.path.join(path, f"{y}.png")
+    resp = requests.get(url, proxies=proxies, stream=True)
+    file_size = int(resp.headers.get('content-length', 0))
+    if os.path.exists(filepath) and file_size != 0 and os.path.getsize(
+            filepath) == file_size:
+        print(f'{filepath} has already download.')
+        return
     if not os.path.exists(path):
         os.makedirs(path)
-    filepath = os.path.join(path, f"{y}.png")
     for x in range(0, 3):
         response = requests.get(url, proxies=proxies)
         if response.status_code == 200:
             with open(filepath, "wb") as f:
                 f.write(response.content)
-            break
+            print(f'{filepath} download success.')
+            return
         else:
-            print("network error!")
+            print(f'{filepath} download failed.')
 
 
 def xyz2lonlat(x, y, z):
@@ -45,35 +55,35 @@ def lonlat2xyz(lon, lat, zoom):
     return int(x), int(y)
 
 
-def cal_tiff_box(x1, y1, x2, y2, z):
-    LT = xyz2lonlat(x1, y1, z)
-    RB = xyz2lonlat(x2 + 1, y2 + 1, z)
-    return Point(LT[0], LT[1]), Point(RB[0], RB[1])
+def latlng_dec2rad(decnum):
+    degree = int(decnum)  #度
+    numdecimal = abs(decnum - degree)
+    tmp = numdecimal * 3600
+    minute = int(tmp // 60)  #分
+    # second = int(tmp - minute * 60)  #秒
+    return f"{degree}_{minute}"
 
 
-def core(z):
-    path = "map"
-    # lt = [-33.8656469, 151.2502086]
-    # rb = [-33.8806469, 151.2652086]
-    ox = [-33.8656469, 151.2502086]
-    lt = [ox[0] + 0.0035, ox[1] - 0.0075]
-    rb = [ox[0] - 0.0035, ox[1] + 0.0075]
-    x1, y1 = lonlat2xyz(lt[1], lt[0], z)
-    x2, y2 = lonlat2xyz(rb[1], rb[0], z)
-    print(x1, y1, z)
-    print(x2, y2, z)
+def map_downloader(ox, zoom=17):
+    lt = [ox[0] + 0.004, ox[1] - 0.008]
+    rb = [ox[0] - 0.004, ox[1] + 0.008]
+    x1, y1 = lonlat2xyz(lt[1], lt[0], zoom)
+    x2, y2 = lonlat2xyz(rb[1], rb[0], zoom)
+    print(x1, y1, zoom)
+    print(x2, y2, zoom)
     count = 0
     all = (x2 - x1 + 1) * (y2 - y1 + 1)
     for i in range(x1, x2 + 1):
         for j in range(y1, y2 + 1):
-            download(i, j, z, path)
+            download(i, j, zoom, map_dir)
             count += 1
             print("{m}/{n}".format(m=count, n=all))
             pass
-    merge(x1, y1, x2, y2, z, path)
+    merge(x1, y1, x2, y2, zoom, map_dir)
 
 
 def merge(x1, y1, x2, y2, z, path):
+    date = time.strftime('%Y%m%d%H%M%S', time.localtime())
     row_list = list()
     for i in range(x1, x2 + 1):
         col_list = list()
@@ -82,8 +92,15 @@ def merge(x1, y1, x2, y2, z, path):
         k = np.vstack(col_list)
         row_list.append(k)
     result = np.hstack(row_list)
-    cv2.imwrite(os.path.join(path, "merge.png"), result)
+    cv2.imwrite(
+        os.path.join(
+            path,
+            f"{latlng_dec2rad(ox[0])}_{latlng_dec2rad(ox[1])}_{date}.png"),
+        result)
 
 
 if __name__ == '__main__':
-    core(z=17)  #调整下载级别
+    # ox = [-33.8656469, 151.2502086]
+    # ox = [-33.8668440, 151.2516450]
+    ox = [49.2473765, -123.1938901]
+    map_downloader(ox, int(sys.argv[1]))
