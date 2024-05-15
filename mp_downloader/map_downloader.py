4b6f9df965516dfa98635f5f2bbc969e4a64c637
siriusmwx <siriusmwx@gmail.com>
2024-05-15 21:46:36 +0800

    Wed May 15 21:46:36     2024

---
+++ mp_downloader/map_downloader.py
@@ -0,0 +1,135 @@
+import os
+import sys
+import time
+import math
+import yaml
+import json
+import requests
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+
+currend_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(currend_dir)
+map_dir = os.path.join(currend_dir, "map")
+
+
+def download(x, y, z, path):
+    proxies = {
+        "http": "socks5h://127.0.0.1:1080",
+        "https": "socks5h://127.0.0.1:1080"
+    }
+    # url = f"https://khms0.google.com/kh/v=979?x={x}&y={y}&z={z}"
+    # url = f"https://mt.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+    url = f"http://www.google.com/maps/vt?lyrs=s@820&gl=cn&x={x}&y={y}&z={z}"
+    path = os.path.join(path, f"{z}{os.sep}{x}")
+    filepath = os.path.join(path, f"{y}.png")
+    try:
+        resp = requests.get(url, proxies=proxies, stream=True)
+        file_size = int(resp.headers.get('content-length', 0))
+        if os.path.exists(filepath) and file_size != 0 and os.path.getsize(
+                filepath) == file_size:
+            print(f'{filepath} has already download.')
+            return True
+        if not os.path.exists(path):
+            os.makedirs(path)
+        print(url)
+        response = requests.get(url, proxies=proxies)
+        if response.status_code == 200:
+            with open(filepath, "wb") as f:
+                f.write(response.content)
+            print(f'{filepath} download success.')
+            return True
+        else:
+            print(f'{filepath} download failed.')
+    except Exception as e:
+        print(e)
+        return False
+    return False
+
+
+def lonlat2xyz(lat, lon, zoom):
+    n = math.pow(2, zoom)
+    x = ((lon + 180) / 360) * n
+    y = (1 - (math.log(
+        math.tan(math.radians(lat)) +
+        (1 / math.cos(math.radians(lat)))) / math.pi)) / 2 * n
+    return int(x), int(y)
+
+
+def convert_point(point):
+    point = abs(point)
+    degree = int(point)  #度
+    numdecimal = point - degree
+    tmp = numdecimal * 3600
+    minute = int(tmp // 60)  #分
+    second = int(tmp - minute * 60)  #秒
+    return f"{degree}°{minute:02}′{second:02.0f}″"
+
+
+def latlng_dec2rad(lt, rb):
+    decnum = [(lt[0] + rb[0]) / 2, (lt[1] + rb[1]) / 2]
+    if decnum[0] < 0:
+        x = "S"
+    else:
+        x = "N"
+    if decnum[1] < 0:
+        y = "W"
+    else:
+        y = "E"
+    return f"{convert_point(decnum[0])}{x} {convert_point(decnum[1])}{y}"
+
+
+def modifyimage(img_path, txt):
+    if isinstance(txt, list):
+        txt = latlng_dec2rad(txt)
+    img = Image.open(img_path)
+    draw = ImageDraw.Draw(img)
+    imgsize = img.size
+    draw_font = ImageFont.truetype('consola.ttf', imgsize[1] // 64)
+    txtsize = draw_font.getbbox(txt)
+    draw.rectangle(
+        [txtsize[3], txtsize[3], txtsize[2] + txtsize[3], 2 * txtsize[3]],
+        fill=(0, 0, 0))
+    draw.text([txtsize[3], txtsize[3]], txt, font=draw_font, fill=(255, 0, 0))
+    img.save(img_path)
+
+
+def merge(x1, y1, x2, y2, z, path):
+    date = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    row_list = list()
+    for i in range(x1, x2 + 1):
+        col_list = list()
+        for j in range(y1, y2 + 1):
+            col_list.append(
+                Image.open(os.path.join(path,
+                                        f"{z}{os.sep}{i}{os.sep}{j}.png")))
+        k = np.vstack(col_list)
+        row_list.append(k)
+    img_path = os.path.join(currend_dir, f"{date}.jpg")
+    final_pic = Image.fromarray(np.hstack(row_list))
+    final_pic.save(img_path)
+    # modifyimage(img_path, font)
+
+
+def map_downloader(ox, zoom=17):
+    x, y = lonlat2xyz(ox[0], ox[1], zoom)
+    x1, y1 = x - 9, y - 4
+    x2, y2 = x + 5, y + 3
+    total = (x2 - x1 + 1) * (y2 - y1 + 1)
+    print(x1, y1, zoom)
+    print(x2, y2, zoom)
+    count = 0
+    try:
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                count += 1
+                print("{m}/{n}".format(m=count, n=total))
+                while not download(i, j, zoom, map_dir):
+                    time.sleep(5)
+        merge(x1, y1, x2, y2, zoom, map_dir)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    map_downloader([44.687319736755526, 33.57245422012606], 17)
